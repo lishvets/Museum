01f447c4a7955d049fc85935b0b78e06f11e9c14
Lidiia Shvets <lepoki051003@gmail.com>
2024-11-09 11:43:10 +0100

    make build

---
+++ index.1433d219.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-undef */document.addEventListener("DOMContentLoaded",function(){// eslint-disable-next-line no-unused-vars
+new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination",clickable:!0},slidesPerView:1,spaceBetween:16,breakpoints:{768:{slidesPerView:2,spaceBetween:20}}})}),document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".icon--menu"),n=document.querySelector(".icon--close"),t=document.querySelector("body");e.addEventListener("click",function(){t.classList.add("menu-open")}),n.addEventListener("click",function(){t.classList.remove("menu-open")})});//# sourceMappingURL=index.1433d219.js.map
+
+//# sourceMappingURL=index.1433d219.js.map
